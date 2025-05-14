f7f104ba0412052f4037f8806efc0b7889b91f5c
marco-ht <marco-ht@outlook.it>
2025-05-14 19:52:03 +0200

    separated test functions

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,33 +1,34 @@
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
-#include "../libft/libft.h"
+# include "../libft/libft.h"
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 
-int	ft_strcmp(const char *s1, const char *s2);
-
-typedef struct	s_exec
+typedef struct s_exec
 {
 	int		type;
 	char	**args;
 	char	**evargs;
-} t_exec;
+}			t_exec;
 
-typedef struct	s_redirect
+typedef struct s_redirect
 {
+}			t_redirect;
 
-} t_redirect;
-
-typedef struct	s_pipe
+typedef struct s_pipe
 {
+}			t_pipe;
 
-} t_pipe;
+void		set_zeros(char *line, t_exec *exec_str);
+void		set_starts(char *line, t_exec *exec_str);
+int			is_whitespace(char c);
 
-void    set_zeros(char *line, t_exec *exec_str);
-void set_starts(char *line, t_exec *exec_str);
+// DEBUG FUNCTIONS
+void		ft_check(t_exec *exec_str);
+void		ft_check_args(t_exec *exec_str);
 
 #endif
--- srcs/parser.c
+++ srcs/parser.c
@@ -5,78 +5,6 @@ int    is_whitespace(char c)
     return(c == 32 || (c >= 9 && c <= 13));
 }
 
-void	ft_check(t_exec *exec_str)
-{
-	int i;
-
-	i = 0;
-	while(exec_str->evargs[i])
-	{
-		printf("*exec_str->evargs[%d] =%d ", i, is_whitespace(*(exec_str->evargs[i])));
-		i++;
-	}
-	printf("\n");
-	i =0;
-	while(exec_str->evargs[i])
-	{
-		printf("exec_str->evargs[%d] =%p ", i, (exec_str->evargs[i]));
-		i++;
-	}
-	printf("\n");
-}
-
-
-void	ft_check_args(t_exec *exec_str)
-{
-	int i;
-
-	printf("\n");
-	printf("\n");
-	i = 0;
-	while(exec_str->args[i])
-	{
-		printf("*exec_str->args[%d] =%d ", i, is_whitespace(*(exec_str->args[i])));
-		i++;
-	}
-	printf("\n");
-	i =0;
-	while(exec_str->args[i])
-	{
-		printf("exec_str->args[%d] =%p ", i, (exec_str->args[i]));
-		i++;
-	}
-	printf("\n");
-}
-
-/* void    set_zeros(char *line, t_exec *exec_str)
-{
-    int i;
-	int	j;
-
-    exec_str->evargs = malloc(100 * sizeof(char *));
-
-	printf("line start = %p ", line);
-	printf("\n");
-    j = 0;
-    while (line[j] && is_whitespace(line[j]))
-		j++;
-	i = 0;
-    while(line[j])
-    {
-		if (!ft_strchr(&line[j], ' '))
-		{
-			exec_str->evargs[i] = &line[j];
-			i++;
-			while (line[j] && is_whitespace(line[j]))
-				j++;
-		}
-		else
-			j++;
-	}
-	exec_str->evargs[i] = NULL;
-	ft_check(exec_str);
-} */
-
 void    set_zeros(char *line, t_exec *exec_str)
 {
     int i;
@@ -123,11 +51,9 @@ void set_starts(char *line, t_exec *exec_str)
     {
         // Start of a word
         exec_str->args[i++] = &line[j];
-
         // Move to the end of the word
         while (line[j] && !is_whitespace(line[j]))
             j++;
-
         // Skip any whitespace between words
         while (line[j] && is_whitespace(line[j]))
             j++;
