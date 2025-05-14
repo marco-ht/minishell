e6649a7de19b2631189dc9689a4c6ed23a814663
marco-ht <marco-ht@outlook.it>
2025-05-14 19:22:52 +0200

    implemented ctrl+d exit

--- srcs/main.c
+++ srcs/main.c
@@ -8,15 +8,14 @@ int main(void)
     while(1)
     {
         str = readline("$ ");
+        if (!str || ft_strncmp(str, "exit", 4) == 0) //useremo la buildin exit, questo e' solo un esempio iniziale
+            break;
         set_zeros(str, &exec_str);
 		set_starts(str, &exec_str);
-        if (ft_strncmp(str, "exit", 4) == 0)    //useremo la buildin exit (che prende anche un parametro facoltativo), questo e' solo un esempio iniziale
-            return(free(str), rl_clear_history(), 0);
-        if (*str)
-            add_history(str);
+        add_history(str);
         printf("Ciao, %s\n", str);
-        free(str);
     }
+    return(free(str), rl_clear_history(), 0);
 }
 /*
 valgrind --leak-check=full --show-leak-kinds=all ./nome_programma
