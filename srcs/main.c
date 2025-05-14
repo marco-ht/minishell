52f796abec0cba6a77bebd7796c9c3c63ce6b66e
marco-ht <marco-ht@outlook.it>
2025-05-14 21:09:26 +0200

    test printf removed

--- srcs/main.c
+++ srcs/main.c
@@ -11,17 +11,13 @@ int main(void)
 		str = readline("$ ");
 		if (!str || ft_strncmp(str, "exit", 4) == 0) //useremo la buildin exit, questo e' solo un esempio iniziale
 			break;
-		/* printf("str = %s\n", str); */
 		set_starts(str, &exec_str);
 		set_zeros(str, &exec_str);
 		add_history(str);
-		/* printf("str = %s\n", *(ft_split(str, ' ')));
-		printf("str = %s\n", *((ft_split(str, ' '))+1)); */
 		matrix = ft_split(str, ' ');
 		if(fork() == 0)
 			execvp(matrix[0], matrix);
 		wait(NULL);
-		/* printf("Ciao, %s\n", str); */
     }
 		return(free(str), rl_clear_history(), 0);
 }
