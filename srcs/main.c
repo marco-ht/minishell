a4c1283355ef826b875dfa768cca4e2ee1624103
marco-ht <marco-ht@outlook.it>
2025-06-10 02:28:44 +0200

    comments main removed

--- srcs/main.c
+++ srcs/main.c
@@ -13,17 +13,8 @@ int main(void)
 	while (ft_getcmd(&buf) >= 0) //! buf (EOF, ctrl+D) -> -1 we stop while loop
 	{
 		add_history(buf);
-
-		/* if (ft_strncmp(buf, "exit", 4) == 0) // useremo la builtin exit, questo e' solo un esempio iniziale
-			break; */
-
-		/* printf("buffer = %s\n", buf); */
-		/* printf("pid main = %d\n", getpid()); */
-		/* tree = ft_tree_builtin2(); */
 		tree = ft_parsecmd(buf);
-		/* if (fork1() == 0) */
 		ft_runcmd(tree);
-		/* wait(NULL); */
 		free_tree(tree);
 		free(buf);
 	}
